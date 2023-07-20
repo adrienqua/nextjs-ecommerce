926f8782823b917dd6c3309d2277e0ea30242c0e
adrienqua <777adrien@gmail.com>
2023-07-20 03:09:06 +0200

    env

--- app/config.js
+++ app/config.js
@@ -1,2 +1,2 @@
-//export const apiUrl = process.env.API_URL
-export const apiUrl = "http://localhost:3000/api/"
+export const apiUrl = process.env.NEXT_PUBLIC_API_URL
+//export const apiUrl = "http://localhost:3000/api/"
